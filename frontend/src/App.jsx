148d4042801898616402ff0e28c008771cdd6e6e
Aviral000 <avimalik02@gmail.com>
2024-05-27 11:53:23 +0530

    shadcn setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='container'>
+      <h2 className="text-3xl font-bold underline">hello</h2>
+    </div>
+  )
+}
